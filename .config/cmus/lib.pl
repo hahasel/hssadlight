e63adeea0e776f33a80e0ccf9d9372fade6590be
hahasel <hannes.fstph.at>
2019-11-05 14:10:51 +0100

    If you can read this, no updates happened to this branch.

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,31 @@
+/home/hannes/music/feuerschwanz/Feuerschwanz - Am Feuer.flac
+/home/hannes/music/feuerschwanz/Feuerschwanz - Bärentanz.flac
+/home/hannes/music/feuerschwanz/Feuerschwanz - Blöde Frage, Saufgelage!.flac
+/home/hannes/music/feuerschwanz/Feuerschwanz - der geile prinz.flac
+/home/hannes/music/feuerschwanz/Feuerschwanz - Der Geizhals.flac
+/home/hannes/music/feuerschwanz/Feuerschwanz - Der Henker.flac
+/home/hannes/music/feuerschwanz/Feuerschwanz - Die Hörner Hoch.flac
+/home/hannes/music/feuerschwanz/Feuerschwanz - Herz im Sturm.flac
+/home/hannes/music/feuerschwanz/Feuerschwanz - Hurra Hurra die Pest is da.flac
+/home/hannes/music/feuerschwanz/Feuerschwanz - Ketzerei.flac
+/home/hannes/music/feuerschwanz/Feuerschwanz - Methämmer.flac
+/home/hannes/music/feuerschwanz/Feuerschwanz - Metnotstand im Märchenland.flac
+/home/hannes/music/feuerschwanz/Feuerschwanz - Moralisch (höchst verwerflich).flac
+/home/hannes/music/feuerschwanz/Feuerschwanz - Operation Drachensturm.flac
+/home/hannes/music/feuerschwanz/Feuerschwanz - ra ra rattenschwanz.flac
+/home/hannes/music/feuerschwanz/Feuerschwanz Schnaps und Schnecken.flac
+/home/hannes/music/feuerschwanz/Feuerschwanz - Schubsetanz.flac
+/home/hannes/music/feuerschwanz/Feuerschwanz - Zuckerbrot und Peitsche.flac
+/home/hannes/music/irish/Flogging Molly - Devils Dance Floor.flac
+/home/hannes/music/irish/Flogging Molly - Drunken Lullabies.flac
+/home/hannes/music/irish/Flogging Molly - Salty Dog.flac
+/home/hannes/music/irish/Flogging Molly - Seven Deadly Sins.flac
+/home/hannes/music/irish/The O'Reillys and the Paddyhats - Barrels of Whiskey.flac
+/home/hannes/music/irish/The O'Reillys and the Paddyhats - Black Sails.flac
+/home/hannes/music/irish/The O'Reillys and the Paddyhats - Fair Old Lady.flac
+/home/hannes/music/irish/The O'Reillys and the Paddyhats - Green Blood.flac
+/home/hannes/music/irish/The O'Reillys and the Paddyhats - Irish Way.flac
+/home/hannes/music/irish/The O'Reillys and the Paddyhats - Paddyhats.flac
+/home/hannes/music/irish/The O'Reillys and the Paddyhats - The Boxer.flac
+/home/hannes/music/irish/The O'Reillys and the Paddyhats - White River.flac
+/home/hannes/music/irish/The Rumjacks - The Pot and Kettle.flac
---
+++ README.md
@@ -0,0 +1,5 @@
+# archi3dotfiles
+Personal i3 configuration for i3
+# archi3dotfiles
+# archi3dotfiles
+hssad light version. Maybe I'll write something here later.
